77151cb24aa564b2a6c55a4971e6831a1fb88568
Ashish <52658931+ashish-sharma-26@users.noreply.github.com>
2022-08-01 12:46:32 +0530

    Update UserController.php
    final

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Exports\DatatblExport;
+use App\Exports\Datatbl2Export;
 use Excel;
+
+
 
 
 class UserController extends Controller
@@ -22,60 +25,42 @@ class UserController extends Controller
     {
         
         $input = $request->all();
-       // echo $input['id'];
         $term = $input['stxt'];
-       // ->where('name','LIKE','%'.$term.'%')
-
-       // print_r($input);die;
-        //$usersinfo = User::where("id",3)->get();
-        //$usersinfo = User::where("id", "=", 104)->get();
+     
         $usersinfo = User::where('name','LIKE','%'.$term.'%')->get();
-// echo "Hello";
-       //print_r($usersinfo);die;
+
         return view("subusers",compact('usersinfo'));
     }
 
     public function ExportajaxData(Request $request)
     {
         
-        
-       
-            // $items = User::all();
-            // Excel::create('items', function($excel) use($items) {
-            // $excel->sheet('ExportFile', function($sheet) use($items) {
-            // $sheet->fromArray($items);
-            // });
-            // })->export('xls');
-        
-        
-        
         $input = $request->all();
+        if(!empty($input['seldata']))
+        {            
+           // $usersinfo = User::whereIn('id', $input['seldata'])->get();
+            return Excel::download(new DatatblExport($input['seldata']), 'datatbl.xlsx');
+        }
+        else{
+            return Excel::download(new Datatbl2Export, 'datatbl.xlsx');
+        }
+       
         //print_r($input['seldata']);die;
-        $usersinfo = User::whereIn('id', $input['seldata'])->get();
-
-        return Excel::download(new DatatblExport($input['seldata']), 'datatbl.xlsx');
-
-        //return Excel::download(new DatatblExport, 'datatbl.xlsx');
         
 
-
-
-    //    $this->ExportData($input['seldata']);
-        // echo "<pre>";
-        // print_r($usersinfo);
-        // die;
     }
 
 
-    public function ExportData($arr) 
+    public function ExportData() 
     {
-        //return Excel::download(new DatatblExport('12'), 'datatbl.xlsx');
-        // print_r($arr);die;
-        // $arr = array(1,2,3);
-        return Excel::download(new DatatblExport($arr), 'datatbl.xlsx');
+        
+        return Excel::download(new Datatbl2Export, 'datatbl.xlsx');
 
-       
-        //return Excel::download((new PayrollHelper)->forMonth($month_start)->forDepartment($department), $file_name, null, [\Maatwebsite\Excel\Excel::XLSX]);
 
     }
+
+
+
+
+
 }
